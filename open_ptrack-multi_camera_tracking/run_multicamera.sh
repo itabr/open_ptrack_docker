3ef15ea33dfac44ee1348f2fe01d4e26ac5a5d97
itabr <itabrz@gmail.com>
2018-08-14 12:12:33 -0700

    run_multicamera.sh updated MACHINE_TYPE added

--- open_ptrack-multi_camera_tracking/run_multicamera.sh
+++ open_ptrack-multi_camera_tracking/run_multicamera.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 export ROS_MASTER_URI="http://192.168.100.101:11311/"
 export ROS_IP="192.168.100.101"
 export ROS_PC_NAME="PC1"
+export MACHINE_TYPE="Server"
 
 echo "Pulling docker image ..."
 sudo docker pull openptrack/open_ptrack && \
@@ -14,7 +16,7 @@ sudo docker run \
 -ti \
 -e DISPLAY \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
--v $(pwd)/open_ptrack_config/Server/etc/ntp.conf/:/etc/ntp.conf \
+-v $(pwd)/open_ptrack_config/$MACHINE_TYPE/etc/ntp.conf/:/etc/ntp.conf \
 --mount type=bind,source=$(pwd)/open_ptrack_config/opt_calibration/launch/,destination=/root/workspace/ros/src/open_ptrack/opt_calibration/launch/ \
 --mount type=bind,source=$(pwd)/open_ptrack_config/opt_calibration/conf/,destination=/root/workspace/ros/src/open_ptrack/opt_calibration/conf/ \
 --mount type=bind,source=$(pwd)/open_ptrack_config/detection/launch/,destination=/root/workspace/ros/src/open_ptrack/detection/launch/ \
